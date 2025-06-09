6b8a99c5c4aa92819ee6452e3bfdfdbc9bdac5a7
Jaiiaj21 <yossawat.ongudomluk@gmail.com>
2025-06-09 20:30:51 +0700

    fix: Wrap useSearchParams in Suspense on home page

--- search-pokemon/src/app/page.tsx
+++ search-pokemon/src/app/page.tsx
@@ -1,10 +1,13 @@
 import PokemonContent from "@/components/PokemonContent";
+import { Suspense } from "react";
 
 export default function Home() {
 
   return (
     <div className="min-h-screen bg-linear-to-b from-[#E0E6EE] to-[#D0D7DE] flex items-center justify-center p-4 font-sans text-gray-800">
-      <PokemonContent />
+      <Suspense fallback={<p>loading...</p>}>
+        <PokemonContent />
+      </Suspense>
     </div>
   );
 }
