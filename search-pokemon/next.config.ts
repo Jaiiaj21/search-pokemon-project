908c9ec7f703f8391ce4197a208fc1591028e97c
Jaiiaj21 <yossawat.ongudomluk@gmail.com>
2025-06-09 20:32:33 +0700

    feat: Update Next.js image config to remotePatterns

--- search-pokemon/next.config.ts
+++ search-pokemon/next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ['img.pokemondb.net'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.pokemondb.net',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
