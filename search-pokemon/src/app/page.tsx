6d3398eeedd7a667ebff008da4e233867cbc86a9
Jaiiaj21 <yossawat.ongudomluk@gmail.com>
2025-06-10 16:22:46 +0700

    fix: Adjust text color for dark mode readability

--- search-pokemon/src/app/page.tsx
+++ search-pokemon/src/app/page.tsx
@@ -5,7 +5,7 @@ import { Suspense } from "react";
 export default function Home() {
 
   return (
-    <div className="min-h-screen bg-fixed bg-gradient-to-b from-red-400 to-white flex items-center justify-center p-4 font-mono">
+    <div className="min-h-screen bg-fixed bg-gradient-to-b from-red-400 to-white flex items-center justify-center p-4 font-mono text-neutral-700">
       <Suspense fallback={<Loading />}>
         <PokemonContent />
       </Suspense>
